4414181bf8078c00313f6d7dddce0aed328193c1
Ewans Angel LS <ewansangel2005@gmail.com>
2026-01-29 15:10:17 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,18 @@
+package pst;
+import java.util.Scanner;
+
+public class leapyear {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the Year");
+		int year=scan.nextInt();
+		if(year%400==0|| year%4==0 && year%100 !=0) {
+			System.out.println("Leap Year");
+		}
+		else {
+			System.out.println("not Leap Year");
+		}
+	}
+
+}
