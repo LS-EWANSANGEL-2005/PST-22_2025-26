c2585218237212c92e31de0a52e7b5f2ae4487fd
Ewans Angel LS <ewansangel2005@gmail.com>
2026-02-05 10:41:41 +0530

    Add files via upload

---
+++ OopsConcept/ParameterAdd.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class ParameterAdd {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println(add(a,b));
+        System.out.println(sub(a,b));
+        System.out.println(mul(a,b));
+        System.out.println(div(a,b));
+	}
+    public static int add(int c,int d) {
+    	return c+d;
+    }
+    public static int sub(int c,int d) {
+    	return c-d;
+    }
+    public static int mul(int c,int d) {
+    	return c*d; 	
+    }
+    public static int div(int c,int d) {
+    	return c/d;
+    } 
+}
