5649bcea62a671eb804956a7c3342717757dd253
Ewans Angel LS <ewansangel2005@gmail.com>
2026-02-05 10:40:38 +0530

    Add files via upload

---
+++ OopsConcept/ExampleCode1.java
@@ -0,0 +1,29 @@
+package oops;
+public class ExampleCode1
+{
+	public static void main(String[] args) {
+		method();
+	}
+	public static void method() {
+	    System.out.println("Dhanya is my soulmate");
+	    Dhanya();
+	}
+	public static void Dhanya() {
+	    System.out.println("Vinothini is a porikki");
+	    Vinothini();
+	}
+	public static void Vinothini() {
+	    System.out.println("SriDevi is Rowdy Girl");
+	    SriDevi();
+	}
+	public static void SriDevi() {
+	    System.out.println("Srikanth is Paithiyam");
+	    Srikanth();
+	}
+	public static void Srikanth() {
+	    System.out.println("Surya is Mental");
+	    Surya();
+	}
+	public static void Surya() {
+	}
+}
