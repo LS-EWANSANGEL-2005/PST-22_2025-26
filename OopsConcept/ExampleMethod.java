dd46ea57c6b8a694ea1d1b9a3be5c2dbbe540d80
Ewans Angel LS <ewansangel2005@gmail.com>
2026-02-05 10:23:53 +0530

    Add files via upload

---
+++ OopsConcept/ExampleMethod.java
@@ -0,0 +1,10 @@
+package oops;
+public class ExampleMethod
+{
+	public static void main(String[] args) { 
+	    method();
+	}
+	public static void method(){
+	    System.out.println("Welcome To Placement Training");
+	}
+}
