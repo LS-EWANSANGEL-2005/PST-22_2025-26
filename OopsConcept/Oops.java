2716616e8fd81ec84591152ca930fb7cc60cd352
Ewans Angel LS <ewansangel2005@gmail.com>
2026-02-05 10:41:04 +0530

    Add files via upload

---
+++ OopsConcept/Oops.java
@@ -0,0 +1,49 @@
+package oops;
+
+public class Oops {
+	
+//main method
+	public static void main(String[] args) {
+	        Oops obj1 = new Oops();
+	        obj1.dhanya();
+	        obj1.ewansangel();
+	        obj1.sri();
+	        
+	        
+	        }
+	//sub method
+	public  void ewansangel() {
+		System.out.println("Good Girl");	
+		}
+	public  void sri() {
+		System.out.println("Ambala da");
+	}
+	public  void vino() {
+		System.out.println("Porikki");
+		
+	}
+	public  void manjima() {
+		System.out.println("Keadi");
+	}
+	public void dhanya() {
+		System.out.println("pallazhagi");
+	}
+
+}
+class ThirdyearIT{
+	public void Surya() {
+		System.out.println("Mental manda");
+	}
+	public void Srikanth() {
+		System.out.println("kirruku");
+	}
+	public void Srihari() {
+		System.out.println("valathavan");
+	}
+	public void Johnisaac() {
+		System.out.println("Mattu Pallu");
+	}
+	
+	
+
+}
